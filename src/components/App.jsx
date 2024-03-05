f572b93e5140c32ce1f690ae4bb12c4bdebf8e12
Mona LisaVladyslav <143653897+VladyslavPunko@users.noreply.github.com>
2024-03-05 23:19:30 +0100

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import Product from "./Product";
+import { Product } from "../components/Product";
 
 export default function App() {
   return (
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,13 @@
-const Product = () => {
+export const Product = () => {
+  const price = 999;
+  const imgUrl =
+    "<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>";
+
   return (
-    <div>
+    <>
       <h2>Tacos</h2>
-      <p>Price: 999 credits</p>
-    </div>
+      <img src={imgUrl} alt="Tacos With Lime" width="640" />
+      <p>Price: {price} credits</p>
+    </>
   );
 };
-
-export default Product;
