eae1f3c69295766cab7103e9d288116a83942007
Mona LisaVladyslav <143653897+VladyslavPunko@users.noreply.github.com>
2024-03-22 23:00:22 +0100

    forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,8 @@
 import "./App.css";
-import { useState } from "react";
+import FeedbackForm from "./Components/Formik/FeedbackForm ";
 
 const App = () => {
-  const [values, setValues] = useState({
-    login: "",
-    password: "",
-  });
-
-  const handleChange = (evt) => {
-    setValues({
-      ...values,
-      [evt.target.name]: evt.target.value,
-    });
-  };
-
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    console.log(values);
-    setValues({
-      login: "",
-      password: "",
-    });
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="login"
-        value={values.login}
-        onChange={handleChange}
-      />
-      <input
-        type="password"
-        name="password"
-        value={values.password}
-        onChange={handleChange}
-      />
-      <button type="submit">Login</button>
-    </form>
-  );
+  return <FeedbackForm />;
 };
 
 export default App;
