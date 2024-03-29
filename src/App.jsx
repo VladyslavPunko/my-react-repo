f353b4a36520d6a9b18b621d5de1331894a14044
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-03-29 21:13:34 +0100

    /

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,49 @@
-import "./App.css";
-import FeedbackForm from "./Components/Formik/FeedbackForm ";
+import { useEffect, useState } from "react";
+import { fetchArticlesWithTopic } from "./articles-api";
+
+const ArticleList = ({ items }) => (
+  <ul>
+    {items.map(({ objectID, url, title }) => (
+      <li key={objectID}>
+        <a href={url} target="_blank" rel="noreferrer noopener">
+          {title}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
 
 const App = () => {
-  return <FeedbackForm />;
-};
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
+  useEffect(() => {
+    async function fetchArticles() {
+      try {
+        setLoading(true);
+				// 2. Використовуємо HTTP-функцію
+				const data = await fetchArticlesWithTopic("react");
+        setArticles(data);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchArticles();
+  }, []);
+
+	return (
+    <div>
+      <h1>Latest articles</h1>
+      {loading && <p>Loading data, please wait...</p>}
+      {error && (
+        <p>Whoops, something went wrong! Please try reloading this page!</p>
+      )}
+      {articles.length > 0 && <ArticleList items={articles} />}
+    </div>
+  );
+};
 export default App;
---
+++ src/Components/HttpZapyty/HttpZapyty.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const ArticleList = ({ items }) => (
+  <ul>
+    {items.map(({ objectID, url, title }) => (
+      <li key={objectID}>
+        <a href={url} target="_blank" rel="noreferrer noopener">
+          {title}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
+
+const App = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    async function fetchArticles() {
+      const response = await axios.get(
+        "https://hn.algolia.com/api/v1/search?query=react"
+      );
+      setArticles(response.data.hits);
+    }
+
+    fetchArticles();
+  }, []);
+
+  return (
+    <div>
+      <h1>Latest articles</h1>
+      {articles.length > 0 && <ArticleList items={articles} />}
+    </div>
+  );
+};
