7056cdc940ba4494c3b0ca29dcb0900b5d8cea65
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-03-29 23:05:09 +0100

    \

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,14 @@
-import { useEffect, useState } from "react";
-import { fetchArticlesWithTopic } from "./articles-api";
-
-const ArticleList = ({ items }) => (
-  <ul>
-    {items.map(({ objectID, url, title }) => (
-      <li key={objectID}>
-        <a href={url} target="_blank" rel="noreferrer noopener">
-          {title}
-        </a>
-      </li>
-    ))}
-  </ul>
-);
+import { useRef, useEffect } from "react";
 
 const App = () => {
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const btnRef = useRef();
 
   useEffect(() => {
-    async function fetchArticles() {
-      try {
-        setLoading(true);
-				// 2. Використовуємо HTTP-функцію
-				const data = await fetchArticlesWithTopic("react");
-        setArticles(data);
-      } catch (error) {
-        setError(true);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    fetchArticles();
+    btnRef.current.focus();
   }, []);
 
-	return (
-    <div>
-      <h1>Latest articles</h1>
-      {loading && <p>Loading data, please wait...</p>}
-      {error && (
-        <p>Whoops, something went wrong! Please try reloading this page!</p>
-      )}
-      {articles.length > 0 && <ArticleList items={articles} />}
-    </div>
-  );
+  return <button ref={btnRef}>Button with forwarded ref</button>;
 };
+
 export default App;
+
